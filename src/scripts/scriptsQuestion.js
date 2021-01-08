fab4e9a115882e6e758ea67f64720b3382654771
Moises-Lopez <a01235387@itesm.mx>
2021-01-08 13:40:40 -0600

    axios 1.0 shuffling options and questions working

--- src/scripts/scriptsQuestion.js
+++ src/scripts/scriptsQuestion.js
@@ -1,7 +1,24 @@
-let newQuestionIndex = () => {
+export let newQuestionIndex = (currentQuestion, questions) => {
   let newIndicePregunta = Math.floor(Math.random() * questions.length);
   while (currentQuestion === newIndicePregunta) {
     newIndicePregunta = Math.floor(Math.random() * questions.length);
   }
   return newIndicePregunta;
 };
+
+export function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+export function shuffleOptions() {
+  let array = [0, 1, 2, 3];
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
