4871c5e23b5c984ffda733a6826da85200df7551
Moises-Lopez <a01235387@itesm.mx>
2021-01-24 22:06:45 -0600

    fixing correct answers count

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -1,11 +1,11 @@
 import React, { Component, useState, useEffect } from "react";
-import {useParams, useHistory} from 'react-router-dom'
+import { useParams, useHistory } from "react-router-dom";
 // import Modal from 'react-modal'
-import Modal from 'react-bootstrap/Modal'
-import axios from 'axios';
+import Modal from "react-bootstrap/Modal";
+import axios from "axios";
 
-import QuestionOption from './questionOption';
-import PopUp from './popUp'
+import QuestionOption from "./questionOption";
+import PopUp from "./popUp";
 
 import "../scripts/scriptsQuestion";
 
@@ -18,16 +18,15 @@ import {
   shuffleOptions,
 } from "./../scripts/scriptsQuestion";
 
-
 const Question = () => {
-  const {theme} = useParams()
-  let history = useHistory()
+  const { theme } = useParams();
+  let history = useHistory();
   const url = `http://localhost:5000/api/questions/${theme}`;
   let [questions, setQuestions] = useState([]);
   let [shuffledOptions, setshuffledOptions] = useState([]);
-  let [correctAnswers, setcorrectAnswers] = useState(0)
-  let [currentCount, setcurrentCount] = useState(1)
-  let [modalValue, setmodalValue] = useState(false)
+  let [correctAnswers, setcorrectAnswers] = useState(0);
+  let [currentCount, setcurrentCount] = useState(1);
+  let [modalValue, setmodalValue] = useState(false);
   let [currentQuestion, setCurrentQuestion] = useState(
     Math.floor(Math.random() * questions.length)
   );
@@ -49,56 +48,54 @@ const Question = () => {
   }
 
   const Message = (correctAnswers) => {
-    console.log(correctAnswers)
-    switch(true) {
-        case (correctAnswers < 3):
-            return 'Te ira mejor a la proxima'
-        case (correctAnswers < 6):
-            return 'Bien!'
-        case (correctAnswers < 8):
-            return 'Felicidades!'
+    console.log(correctAnswers);
+    switch (true) {
+      case correctAnswers < 3:
+        return "Te ira mejor a la proxima";
+      case correctAnswers < 6:
+        return "Bien!";
+      case correctAnswers < 8:
+        return "Felicidades!";
     }
-  }
-
+  };
 
   const handleCloseModal = () => {
-    console.log('hello modal')
-    setmodalValue(false)
-    history.push('/Home')
-  }
+    console.log("hello modal");
+    setmodalValue(false);
+    history.push("/Home");
+  };
 
   const handleOpenModal = () => {
-      setmodalValue(true)
-  }
+    setmodalValue(true);
+  };
 
   let question = questions[currentQuestion];
   let multipleChoices = question.multipleChoice;
 
-  let usedQuestions = []
+  let usedQuestions = [];
 
   const handleSelectedOption = (idOption) => {
     let newCurrentClasses = ["", "", "", ""];
     let emptyClasses = ["", "", "", ""];
     if (0 === idOption) {
       newCurrentClasses[idOption] = "correct";
-      setcorrectAnswers(correctAnswers++)
+      setcorrectAnswers(correctAnswers + 1);
     } else {
       newCurrentClasses[idOption] = "wrong";
     }
     setCurrentClasses(newCurrentClasses);
 
     if (currentCount < totalQuestions) {
-    usedQuestions.push(currentQuestion);
-    let newIndicePregunta = newQuestionIndex(questions, usedQuestions);
+      usedQuestions.push(currentQuestion);
+      let newIndicePregunta = newQuestionIndex(questions, usedQuestions);
 
-    setTimeout(() => {
-      setCurrentClasses(emptyClasses);
-      setCurrentQuestion(newIndicePregunta);
-      setcurrentCount(currentCount + 1)
-    }, 500);
-
+      setTimeout(() => {
+        setCurrentClasses(emptyClasses);
+        setCurrentQuestion(newIndicePregunta);
+        setcurrentCount(currentCount + 1);
+      }, 500);
     } else {
-      handleOpenModal()
+      handleOpenModal();
     }
   };
 
@@ -154,7 +151,9 @@ const Question = () => {
   let questionDiv = (
     <div>
       <div className="flex-container">
-        <div className="title">{question.question}  {` ${currentCount} / ${totalQuestions}`}</div>
+        <div className="title">
+          {question.question} {` ${currentCount} / ${totalQuestions}`}
+        </div>
         {choicesDiv}
       </div>
       {/* <Modal
@@ -170,18 +169,18 @@ const Question = () => {
         correctAnswers={correctAnswers}
         />
         </Modal> */}
-        <Modal show={modalValue} onHide={handleCloseModal}>
+      <Modal show={modalValue} onHide={handleCloseModal}>
         <Modal.Header closeButton>
           <Modal.Title>{Message(correctAnswers)}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <PopUp
-          handleCloseModal={handleCloseModal}
-          totalQuestions={totalQuestions}
-          correctAnswers={correctAnswers}
+            handleCloseModal={handleCloseModal}
+            totalQuestions={totalQuestions}
+            correctAnswers={correctAnswers}
           />
         </Modal.Body>
-        </Modal>
+      </Modal>
     </div>
   );
 
